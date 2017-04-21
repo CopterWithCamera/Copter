c7af73777f1d9f3758f9d3ca65c059853ea5a02a
shuanglengyunji <liuhan211211@gmail.com>
2017-04-21 13:08:15 +0800

    添加usart1代码

--- drivers/usart.c
+++ drivers/usart.c
@@ -402,5 +402,129 @@ int fgetc(FILE *f)
 	return (int)USART_ReceiveData(UART4);
 }
 
+//========================================================================================================
+
+
+//========================================================================================================
+//USART2
+
+void Usart1_Init(u32 br_num)
+{
+	USART_InitTypeDef USART_InitStructure;
+	USART_ClockInitTypeDef USART_ClockInitStruct;
+	NVIC_InitTypeDef NVIC_InitStructure;
+	GPIO_InitTypeDef GPIO_InitStructure;
+	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE); //开启USART2时钟
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+	
+	//串口中断优先级
+	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);	
+
+	
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource5, GPIO_AF_USART2);
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource6, GPIO_AF_USART2);
+	
+	//配置PD5作为USART2　Tx
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5; 
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP ;
+	GPIO_Init(GPIOD, &GPIO_InitStructure); 
+	//配置PD6作为USART2　Rx
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 ; 
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL ;
+	GPIO_Init(GPIOD, &GPIO_InitStructure); 
+	
+	//配置USART2
+	//中断被屏蔽了
+	USART_InitStructure.USART_BaudRate = br_num;       //波特率可以通过地面站配置
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;  //8位数据
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;   //在帧结尾传输1个停止位
+	USART_InitStructure.USART_Parity = USART_Parity_No;    //禁用奇偶校验
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None; //硬件流控制失能
+	USART_InitStructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;  //发送、接收使能
+	//配置USART2时钟
+	USART_ClockInitStruct.USART_Clock = USART_Clock_Disable;  //时钟低电平活动
+	USART_ClockInitStruct.USART_CPOL = USART_CPOL_Low;  //SLCK引脚上时钟输出的极性->低电平
+	USART_ClockInitStruct.USART_CPHA = USART_CPHA_2Edge;  //时钟第二个边沿进行数据捕获
+	USART_ClockInitStruct.USART_LastBit = USART_LastBit_Disable; //最后一位数据的时钟脉冲不从SCLK输出
+	
+	USART_Init(USART2, &USART_InitStructure);
+	USART_ClockInit(USART2, &USART_ClockInitStruct);
+
+	//使能USART2接收中断
+	USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
+	//使能USART2
+	USART_Cmd(USART2, ENABLE); 
+//	//使能发送（进入移位）中断
+//	if(!(USART2->CR1 & USART_CR1_TXEIE))
+//	{
+//		USART_ITConfig(USART2, USART_IT_TXE, ENABLE); 
+//	}
+
+
+}
+
+u8 TxBuffer1[256];	//这里必须是256，因为计数变量使u8的，加到255后自动回0
+u8 TxCounter1=0;	//发送指针
+u8 count1=0; 		//数据存入指针
+
+u8 Rx_Buf1[256];	//串口接收缓存
+
+void Usart1_IRQ(void)
+{
+	u8 com_data;
+	
+	if(USART2->SR & USART_SR_ORE)//ORE中断
+	{
+		com_data = USART2->DR;
+	}
+
+	//接收中断
+	if( USART_GetITStatus(USART2,USART_IT_RXNE) )
+	{
+		USART_ClearITPendingBit(USART2,USART_IT_RXNE);//清除中断标志
+
+		com_data = USART2->DR;
+		ANO_DT_Data_Receive_Prepare(com_data);
+	}
+	//发送（进入移位）中断
+	if( USART_GetITStatus(USART2,USART_IT_TXE ) )
+	{
+				
+		USART2->DR = TxBuffer[TxCounter++]; //写DR清除中断标志          
+		if(TxCounter == count)
+		{
+			USART2->CR1 &= ~USART_CR1_TXEIE;		//关闭TXE（发送中断）中断
+		}
+
+		//USART_ClearITPendingBit(USART2,USART_IT_TXE);
+	}
+}
+
+void Usart1_Send(unsigned char *DataToSend ,u8 data_num)
+{
+	u8 i;
+	for(i=0;i<data_num;i++)
+	{
+		TxBuffer[count++] = *(DataToSend+i);
+	}
+
+	if(!(USART2->CR1 & USART_CR1_TXEIE))
+	{
+		USART_ITConfig(USART2, USART_IT_TXE, ENABLE); //打开发送中断
+	}
+
+}
+
 /******************* (C) COPYRIGHT 2014 ANO TECH *****END OF FILE************/
 
