f5e6c910dd4c48e49b268b9935cfdaf3215b9723
shuanglengyunji <liuhan211211@gmail.com>
2017-05-23 10:32:52 +0800

    更换传感器为US100，增加辅助信息回传

--- applications/data_transfer.c
+++ applications/data_transfer.c
@@ -56,7 +56,6 @@ static void ANO_DT_Send_Check(u8 head, u8 check_sum)
 	data_to_send[4]=head;
 	data_to_send[5]=check_sum;
 	
-	
 	u8 sum = 0;
 	for(u8 i=0;i<6;i++)
 		sum += data_to_send[i];
@@ -172,6 +171,7 @@ void ANO_DT_Data_Exchange(void)
 	{
 		f.send_user = 0;
 		ANO_DT_Send_User();
+		ANO_DT_Send_User2();
 	}
 /////////////////////////////////////////////////////////////////////////////////////
 	else if(f.send_senser)
@@ -300,7 +300,7 @@ void ANO_DT_Data_Receive_Prepare(u8 data)
 	else if(state==3&&data<50)
 	{
 		state = 4;
-		RxBuffer[3]=data;
+		RxBuffer[3]=data;	//Len存入数组
 		_data_len = data;
 		_data_cnt = 0;
 	}
@@ -308,13 +308,13 @@ void ANO_DT_Data_Receive_Prepare(u8 data)
 	{
 		_data_len--;
 		RxBuffer[4+_data_cnt++]=data;
-		if(_data_len==0)
+		if(_data_len==0)	//按照_data_len长度把数据存入数组
 			state = 5;
 	}
 	else if(state==5)
 	{
 		state = 0;
-		RxBuffer[4+_data_cnt]=data;
+		RxBuffer[4+_data_cnt]=data;		//SUM位
 		ANO_DT_Data_Receive_Anl(RxBuffer,_data_cnt+5);
 	}
 	else
@@ -967,5 +967,38 @@ void ANO_DT_Send_User()
 	ANO_DT_Send_Data(data_to_send, _cnt);
 }
 
+//向地面站辅助程序发送信息
+void ANO_DT_Send_User2()
+{
+	u8 _cnt=0;
+	vs16 _temp;
+	
+	data_to_send[_cnt++]=0xAA; 
+	data_to_send[_cnt++]=0xAA;
+	data_to_send[_cnt++]=0xf2; //用户数据2（对地面站辅助程序）
+	data_to_send[_cnt++]=0;
+	
+	_temp = (s16)mode_state;				//飞行模式
+	data_to_send[_cnt++]=BYTE1(_temp);
+	data_to_send[_cnt++]=BYTE0(_temp);
+
+	_temp = (s16)ctrl_command;				//指令编号
+	data_to_send[_cnt++]=BYTE1(_temp);
+	data_to_send[_cnt++]=BYTE0(_temp);
+	
+	_temp = (s16)fly_ready;					//解锁状态
+	data_to_send[_cnt++]=BYTE1(_temp);
+	data_to_send[_cnt++]=BYTE0(_temp);
+	
+	data_to_send[3] = _cnt-4;				//LEN位，在这里补上
+	
+	u8 sum = 0;
+	for(u8 i=0;i<_cnt;i++)
+		sum += data_to_send[i];
+	
+	data_to_send[_cnt++]=sum;
+
+	ANO_DT_Send_Data(data_to_send, _cnt);
+}
 
 /******************* (C) COPYRIGHT 2014 ANO TECH *****END OF FILE************/
--- applications/fly_mode.c
+++ applications/fly_mode.c
@@ -7,7 +7,7 @@ u8 mode_state;
 void mode_check(float *ch_in)
 {
 	/*
-	mode_state：
+	mode_state:
 	0：手动
 	1：气压计
 	2：超声波+气压计
@@ -47,26 +47,3 @@ void mode_check(float *ch_in)
 	Ctrl_Mode(ch_in);
 }
 
-
-
-//旧的模式判断代码，0 -- 手动，1 -- 气压计， 2 -- 超声波
-//u8 mode_state_old;
-//void mode_check(float *ch_in)
-//{
-//	//根据AUX1通道（第5通道）的数值切换飞行模式
-//	if(*(ch_in+AUX1) <-200)			//最低
-//	{
-//		mode_state = 0;	//手动油门
-//	}
-//	else if(*(ch_in+AUX1) >200)		//中间
-//	{
-//		mode_state = 2;	//超声波+气压计融合
-//	}
-//	else							//最高
-//	{
-//		mode_state = 1;	//气压计定高
-//	}
-//	
-//	//===========   ===========
-//	mode_state_old = mode_state; //历史模式
-//}
