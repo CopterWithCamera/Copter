2bf0a309034c62cf1760970c1a5209396c03331c
shuanglengyunji <liuhan211211@gmail.com>
2017-07-11 14:56:42 +0800

    PID初步封装结束

--- applications/height_ctrl.c
+++ applications/height_ctrl.c
@@ -69,126 +69,72 @@ void h_pid_init()
 	
 }
 
-float thr_set,thr_pid_out,thr_out,thr_take_off,tilted_fix;
+/*
+	高度PID运算函数
 
-float en_old;
-u8 ex_i_en;
-float Height_Ctrl(float T,float thr,u8 ready,float en)	//en	1：定高   0：非定高
+	输入：
+	T：调用时间间隔
+	en：PID积分使能  en = 1 时 PID中的积分变量开始积分
+	height_error：期望高度差，后期会改为期望高度
+	takeoff_flag：起飞处理标志，置1时执行起飞处理（只有起飞瞬间所在周期被置1）
+	modechange_flag：飞行中切入定高模式（只有与手动油门模式切换时有用）
+	
+	输出：
+	thr_out：	油门输出值
+*/
+float thr_pid_out,	//微调PID输出
+	thr_take_off,	//基准值输出
+	tilted_fix,		//补偿（融合）系数
+	thr_out;	//高度PID最终输出油门值
+float Height_Pid(float T,float en,float height_error,u8 takeoff_flag,u8 modechange_flag)
 {
-	//thr：0 -- 1000
-	static u8 speed_cnt,height_cnt;
+	static u8 speed_cnt,height_cnt;		//速度、位置PID调用周期计数变量
 	
-//=====================================================================================================
-//				模式切换与模式识别，在未解锁时默认为手动模式，防止自动模式开环运行
-//=====================================================================================================
-	
-	//解锁情况判断，用于选择后续代码是否执行
-	if(ready == 0)	//没有解锁（已经上锁）
+	//0.飞行中切入定高模式处理	//处理飞行过程中突然进入此模式的情况
+	if(modechange_flag)		
 	{
-		en = 0;						//转换为手动模式，禁止自动定高代码的执行
-		thr_take_off_f = 0;			//起飞标志清零
-		thr_take_off = 0;			//基准油门清零
-	}
-	
-	//模式判断
-	if(en < 0.1f)		
-	{
-		//=====================================================================================================
-		//										手动模式
-		//=====================================================================================================
-		
-		en_old = en;	//更新历史模式
-		
-		return (thr);	//thr是传入的油门值，thr：0 -- 1000
-						//把传入油门直接传出去了，上面所有算法都没用上
-	}
-	
-//=====================================================================================================
-//								自动模式处理
-//=====================================================================================================
-
-	//油门处理：
-	//取值范围转换、设置死区
-	//thr_set是经过死区设置的油门控制量输入值，取值范围 -500 -- +500
-	thr_set = my_deathzoom_2(my_deathzoom((thr - 500),0,40),0,10);	//±50为死区，零点为±40的位置
-
-	//模式切换处理
-	//飞行中初次进入定高模式切换处理（安全保护，防止基准油门过低）
-	if( ABS(en - en_old) > 0.5f )	//从非定高切换到定高（官方注释）	//我认为是模式在飞行中被切换，切换方向不确定
-	{
-		
-		if(thr_take_off<10)			//未计算起飞油门（官方注释）
+		if(thr_take_off<10)			//未计算起飞油门（官方注释）    //如果油门没有基准值,则手动设定油门基准值
 		{
-			//thr_set是经过死区设置的油门控制量输入值，取值范围 -500 -- +500。
-			//这个判断有可能造成在地面上解锁后推一点油门时，切换模式导致飞机一下子窜上天（比定高起飞还快）
-			if(thr_set > -150)		//thr_set > -150 代表油门非低
-			{
-				thr_take_off = 400;
-			}
-		}
-		en_old = en;	//更新历史模式
-	}
-	
-	//======================================================================================
-
-	//遥控器输入值控制升降
-	
-	//生成期望速度
-	if(thr_set>0)	//上升
-	{
-		set_speed_t = thr_set/450 * MAX_VERTICAL_SPEED_UP;	//set_speed_t 表示期望上升速度占最大上升速度的比值
-		
-		//遥控器控制条件下的起飞处理
-		if(thr_take_off_f == 0)	//如果没有起飞（本次解锁后还没有起飞）
-		{
-			if(thr_set>100)	//达到起飞油门
-			{
-				thr_take_off_f = 1;	//起飞标志置1，此标志只在上锁后会被归零
-				thr_take_off = 350; //直接赋值起飞基准油门
-			}
+				thr_take_off = 400;	//设置基准油门
 		}
 	}
-	else			//悬停或下降
+	
+	//0.起飞处理
+	if(takeoff_flag)
 	{
-		set_speed_t = thr_set/450 * MAX_VERTICAL_SPEED_DW;
+		thr_take_off = 350;
 	}
+	
+	
+	//1.微调PID部分（输入的是目标高度差）
+	
+	//位置（高度）PID（输入目标高度差，输出期望速度）
+	static float dT2;
+	dT2 += T;		//计算微分时间
+	height_cnt++;	//计算循环执行周期
+	if(height_cnt>=100)  //200ms（调用间隔2ms）
+	{
+		
 
-	//速度期望限幅滤波
-	set_speed_t = LIMIT(set_speed_t,-MAX_VERTICAL_SPEED_DW,MAX_VERTICAL_SPEED_UP);	//速度期望限幅
-	LPF_1_(10.0f,T,my_pow_2_curve(set_speed_t,0.25f,MAX_VERTICAL_SPEED_DW),set_speed);	//LPF_1_是低通滤波器，截至频率是10Hz，输出值是set_speed，my_pow_2_curve把输入数据转换为2阶的曲线，在0附近平缓，在数值较大的部分卸率大
-	set_speed = LIMIT(set_speed,-MAX_VERTICAL_SPEED_DW,MAX_VERTICAL_SPEED_UP);	//限幅，单位mm/s
-
-	//set_speed 为最终输出的期望速度
+		//位置PID
+		//输入的反馈值是高度差而不是高度，相当于已经把error输入了，所以期望值为0时正好是 error = error - 0
+		exp_speed = PID_calculate( 		dT2,            //周期
+										0,				//前馈
+										0,				//期望值（设定值）
+										//-set_height_e,	//反馈值				set_height_e：期望高度差，单位mm
+										-height_error,	//反馈值				set_height_e：期望高度差，单位mm
+										&h_height_arg, 	//PID参数结构体
+										&h_height_val,	//PID数据结构体
+										1500 *en		//integration limit，积分限幅
+								 );			//输出	
+		exp_speed = LIMIT(exp_speed,-300,300);
+		
+		height_cnt = 0;
+		dT2 = 0;
+	}
 	
-	//======================================================================================
-	
-	//生成期望高度差
-	
-	//高度差 = ∑速度差*T （单位 mm/s）
-	//h(n) = h(n-1) + △h  ， △h =（期望速度 - 当前速度） * △t
-	//用解锁状态给目标高度差积分控制变量赋值，只有在ex_i_en = 1时才会开始积分计算目标高度差
-	ex_i_en = thr_take_off_f;
-	
-	set_height_em += (set_speed -        hc_value.m_speed)      * T;	//没有经过加速度修正和带通滤波的速度值算出的速度差 * △T
-	set_height_em = LIMIT(set_height_em,-5000 *ex_i_en,5000 *ex_i_en);	//ex_i_en = 1 表示已经到达起飞油门，否则为0
-	
-	set_height_e += (set_speed  - 1.05f *hc_value.fusion_speed) * T;	//经过加速度修正和带通滤波的速度值算出的速度差 * △T
-	set_height_e  = LIMIT(set_height_e ,-5000 *ex_i_en,5000 *ex_i_en);
-	
-	LPF_1_(0.0005f,T,set_height_em,set_height_e);	//频率 时间 输入 输出	//两个速度差按比例融合，第一个参数越大，set_height_em的占比越大	
-	
-	//至此得出高度差 set_height_e ，单位 mm
-	
-//***********************************************************************************************************************************
-//										PID部分（输入的是目标高度差）
-//***********************************************************************************************************************************
-
-//========================================================================================================
-//										速度PID（输入期望速度，输出期望加速度）
-//========================================================================================================
-	
-	static float dT,dT2;
-	
+	//速度PID（输入期望速度，输出期望加速度）
+	static float dT;
 	dT += T;
 	speed_cnt++;
 	if(speed_cnt>=10) //u8  20ms
@@ -202,47 +148,17 @@ float Height_Ctrl(float T,float thr,u8 ready,float en)	//en	1：定高   0：非定高
 								500 *en						//integration limit，积分限幅
 								 );							//输出
 		
-		exp_acc = LIMIT(exp_acc,-3000,3000);
+		exp_acc = LIMIT(exp_acc,-3000,3000);	
 		
-		//===============================================================================
-		//					位置PID（输入期望高度（也可以是期望高度差），输出期望速度）
-		//===============================================================================
-		
-		dT2 += dT;		//计算微分时间
-		height_cnt++;	//计算循环执行周期
-		if(height_cnt>=10)  //200ms 
-		{
-			height_cnt = 0;
-			
-			//位置PID
-			//输入的反馈值是高度差而不是高度，相当于已经把error输入了，所以期望值为0时正好是 error = error - 0
-			exp_speed = PID_calculate( 		dT2,            //周期
-											0,				//前馈
-											0,				//期望值（设定值）
-											-set_height_e,	//反馈值				set_height_e：期望高度差，单位mm
-											&h_height_arg, 	//PID参数结构体
-											&h_height_val,	//PID数据结构体
-											1500 *en		//integration limit，积分限幅
-									 );			//输出	
-			exp_speed = LIMIT(exp_speed,-300,300);
-			
-			dT2 = 0;
-		}
-		
-		//===============================================================================
-		
-		//速度PID的累加变量清零
 		dT = 0;
 		speed_cnt = 0;
 	}
 	
-//========================================================================================================
-//										加速度PID
-//========================================================================================================		
+	
+	//加速度PID部分
 	float acc_i_lim;
 	acc_i_lim = safe_div(150,h_acc_arg.ki,0);		//acc_i_lim = 150 / h_acc_arg.ki
 													//避免除零错误（如果出现除零情况，就得0）
-	
 	//计算加速度
 	fb_speed_old = fb_speed;						//存储上一次的速度
 	fb_speed = hc_value.fusion_speed;				//读取当前速度
@@ -259,10 +175,15 @@ float Height_Ctrl(float T,float thr,u8 ready,float en)	//en	1：定高   0：非定高
 								 &h_acc_val,		//PID数据结构体
 								 acc_i_lim*en		//integration limit，积分限幅     如果在手动模式，en = 0，这个结果就是0了
 								);					//输出
-								
-//========================================================================================================
-//							基准油门调整（防止积分饱和过深）（基于加速度PID的Error）
-//========================================================================================================
+
+
+	//（微调PID部分结束，输出结果thr_pid_out）			
+
+
+
+	//2.基准油门修正部分
+
+	//基准油门调整（防止积分饱和过深）（基于加速度PID的Error）
 	if(h_acc_val.err_i > (acc_i_lim * 0.2f))
 	{
 		if(thr_take_off<THR_TAKE_OFF_LIMIT)
@@ -279,13 +200,13 @@ float Height_Ctrl(float T,float thr,u8 ready,float en)	//en	1：定高   0：非定高
 			h_acc_val.err_i += safe_div(150,h_acc_arg.ki,0) *T;
 		}
 	}
-	thr_take_off = LIMIT(thr_take_off,0,THR_TAKE_OFF_LIMIT); //限幅	
-
-//***********************************************************************************************************************************
+	thr_take_off = LIMIT(thr_take_off,0,THR_TAKE_OFF_LIMIT); //限幅
 	
-//========================================================================================================	
-//										油门补偿、油门输出
-//========================================================================================================
+
+	//（基准油门修正部分结束，输出结果thr_take_off）
+
+
+	//	3.油门补偿、油门输出整合部分
 	
 	/*
 			这个公式的原型是：
@@ -326,11 +247,126 @@ float Height_Ctrl(float T,float thr,u8 ready,float en)	//en	1：定高   0：非定高
 	thr_out = (thr_pid_out + tilted_fix *(thr_take_off) );	//由两部分组成：油门PID + 油门补偿 * 起飞油门
 	thr_out = LIMIT(thr_out,0,1000);
 
-//========================================================================================================
-//										结果输出
-//========================================================================================================
+	//（输出整合部分结束，输出结果为thr_out）
 
-	return (thr_out);	//经过定高运算的油门值
+	return thr_out;
+}
+
+float Height_Ctrl(float T,float thr,u8 ready,float en)	//高度控制使能：   en	1 -- 定高   0 -- 非定高
+{
+	static float en_old;	//高度控制使能变量 en 的历史模式
+	u8 detection_modechange_flag = 0;	//飞行中检测到模式切换则置位
+	u8 detection_takeoff_flag = 0;		//检测到起飞则置位
+	//thr：0 -- 1000
+
+	/*模式切换与模式识别，在未解锁时默认为手动模式，防止自动模式开环运行*/
+	
+	//解锁情况判断，用于选择后续代码是否执行
+	if(ready == 0)	//没有解锁（已经上锁）
+	{
+		en = 0;						//转换为手动模式，禁止自动定高代码的执行
+		thr_take_off_f = 0;			//起飞标志清零
+		thr_take_off = 0;			//基准油门清零
+	}
+	
+	//模式判断
+	if(en < 0.1f)		
+	{
+		
+		/*手动模式*/
+		
+		en_old = en;	//手动模式下en_old更新历史模式
+		
+		return (thr);	//thr是传入的油门值，thr：0 -- 1000
+						//把传入油门直接传出去了，上面所有算法都没用上
+	}
+	
+
+	/*自动模式*/
+
+
+	//油门处理：
+	
+	//取值范围转换、设置死区
+	//thr_set是经过死区设置的油门控制量输入值，取值范围 -500 -- +500
+	float thr_set;	//本函数处理后的油门
+	thr_set = my_deathzoom_2(my_deathzoom((thr - 500),0,40),0,10);	//±50为死区，零点为±40的位置
+
+	//======================================================================================
+	//状态检测
+	
+	//模式切换检测
+	//飞行中初次进入定高模式切换处理（安全保护，防止基准油门过低）
+	if( ABS(en - en_old) > 0.5f )	//从非定高切换到定高（官方注释）	//我认为是模式在飞行中被切换，切换方向不确定
+	{
+
+		//thr_set > -150 代表油门非低
+		if(thr_set > -150)		//thr_set是经过死区设置的油门控制量输入值，取值范围 -500 -- +500
+		{
+			//thr_take_off = 400;	//设置基准油门
+			detection_modechange_flag = 1;	//飞行中切入定高模式
+		}
+
+		en_old = en;	//更新历史模式
+	}
+	
+	//起飞检测
+	if(thr_set>0)	//油门推过中值
+	{
+		if(thr_take_off_f == 0)	//如果没有起飞（本次解锁后还没有起飞）
+		{
+			if(thr_set>100)	//达到起飞油门
+			{
+				thr_take_off_f = 1;	//起飞标志置1，此标志只在上锁后会被归零
+				detection_takeoff_flag = 1;		//检测到起飞
+			}
+		}
+	}
+
+	//======================================================================================
+	//遥控器输入值控制升降
+	
+	//生成期望速度
+	if(thr_set>0)	//上升
+	{
+		set_speed_t = thr_set/450 * MAX_VERTICAL_SPEED_UP;	//set_speed_t 表示期望上升速度占最大上升速度的比值
+	}
+	else			//悬停或下降
+	{
+		set_speed_t = thr_set/450 * MAX_VERTICAL_SPEED_DW;
+	}
+
+	//速度期望限幅滤波
+	set_speed_t = LIMIT(set_speed_t,-MAX_VERTICAL_SPEED_DW,MAX_VERTICAL_SPEED_UP);	//速度期望限幅
+	LPF_1_(10.0f,T,my_pow_2_curve(set_speed_t,0.25f,MAX_VERTICAL_SPEED_DW),set_speed);	//LPF_1_是低通滤波器，截至频率是10Hz，输出值是set_speed，my_pow_2_curve把输入数据转换为2阶的曲线，在0附近平缓，在数值较大的部分卸率大
+	set_speed = LIMIT(set_speed,-MAX_VERTICAL_SPEED_DW,MAX_VERTICAL_SPEED_UP);	//限幅，单位mm/s
+
+	//set_speed 为最终输出的期望速度
+	
+	//生成期望高度差
+	
+	//高度差 = ∑速度差*T （单位 mm/s）
+	//h(n) = h(n-1) + △h  ， △h =（期望速度 - 当前速度） * △t
+	//用起飞状态给目标高度差积分控制变量赋值，只有在ex_i_en = 1时才会开始积分计算目标高度差
+	static u8 ex_i_en;		//期望高度差控制变量（只有起飞后才会开始计数期望高度差）
+	ex_i_en = thr_take_off_f;
+	
+	set_height_em += (set_speed -        hc_value.m_speed)      * T;	//没有经过加速度修正和带通滤波的速度值算出的速度差 * △T
+	set_height_em = LIMIT(set_height_em,-5000 *ex_i_en,5000 *ex_i_en);	//ex_i_en = 1 表示已经到达起飞油门，否则为0
+	
+	set_height_e += (set_speed  - 1.05f *hc_value.fusion_speed) * T;	//经过加速度修正和带通滤波的速度值算出的速度差 * △T
+	set_height_e  = LIMIT(set_height_e ,-5000 *ex_i_en,5000 *ex_i_en);
+	
+	LPF_1_(0.0005f,T,set_height_em,set_height_e);	//频率 时间 输入 输出	//两个速度差按比例融合，第一个参数越大，set_height_em的占比越大	
+	
+	//set_height_e 为期望高度差，单位 mm
+	//======================================================================================
+	
+	//高度控制PID
+	float my_thr_out;
+	my_thr_out = Height_Pid(T,en,set_height_e,detection_takeoff_flag,detection_modechange_flag);	
+
+	return (my_thr_out);	//经过定高运算的油门值
 
 }
 
