bc6592f54791e3708a3e20366f48d29fc9a1a110
shuanglengyunji <liuhan211211@gmail.com>
2017-07-12 19:18:47 +0800

    向Camera发送高度数据

--- applications/camera_datatransfer.c
+++ applications/camera_datatransfer.c
@@ -1,8 +1,67 @@
 #include "camera_datatransfer.h"
 #include "usart.h"
+#include "anotc_baro_ctrl.h"
+
+/*
+
+高度数据：
+
+ultra.relative_height					cm		float
+sonar.displacement						mm		float
+sonar_fusion.fusion_displacement.out	mm		float
+
+*/
+
+#define BYTE0(dwTemp)       ( *( (char *)(&dwTemp)	  ) )
+#define BYTE1(dwTemp)       ( *( (char *)(&dwTemp) + 1) )
+#define BYTE2(dwTemp)       ( *( (char *)(&dwTemp) + 2) )
+#define BYTE3(dwTemp)       ( *( (char *)(&dwTemp) + 3) )
+
+unsigned char Data_Buffer[20];
+
+void Send_to_Camera(unsigned char *DataToSend ,u8 data_num)
+{
+	Usart3_Send(DataToSend,data_num);
+}
+
+void Camrea_Send_Height(void)
+{
+	float tmp_f;
+	
+	u8 cnt = 0;
+	
+	//帧头
+	Data_Buffer[cnt++] = 0xAA;	
+	Data_Buffer[cnt++] = 0xAA;
+	
+	//功能字
+	Data_Buffer[cnt++] = 0x01;	
+	
+	//内容
+	tmp_f = ultra.relative_height*10;	//转为mm单位
+	Data_Buffer[cnt++] = BYTE3(tmp_f);
+	Data_Buffer[cnt++] = BYTE2(tmp_f);
+	Data_Buffer[cnt++] = BYTE1(tmp_f);
+	Data_Buffer[cnt++] = BYTE0(tmp_f);
+	
+	tmp_f = sonar.displacement;
+	Data_Buffer[cnt++] = BYTE3(tmp_f);
+	Data_Buffer[cnt++] = BYTE2(tmp_f);
+	Data_Buffer[cnt++] = BYTE1(tmp_f);
+	Data_Buffer[cnt++] = BYTE0(tmp_f);
+	
+	tmp_f = sonar_fusion.fusion_displacement.out;
+	Data_Buffer[cnt++] = BYTE3(tmp_f);
+	Data_Buffer[cnt++] = BYTE2(tmp_f);
+	Data_Buffer[cnt++] = BYTE1(tmp_f);
+	Data_Buffer[cnt++] = BYTE0(tmp_f);
+	
+	Send_to_Camera(Data_Buffer,cnt-1);
+}
+
 
 void Camera_Data_Send(void)
 {
-	unsigned char a[2] = {1,2};
-	Usart3_Send(a,2);
+	Camrea_Send_Height();
 }
+
--- applications/data_transfer.c
+++ applications/data_transfer.c
@@ -25,7 +25,7 @@
 
 /////////////////////////////////////////////////////////////////////////////////////
 //数据拆分宏定义，在发送大于1字节的数据类型时，比如int16、float等，需要把数据拆分成单独字节进行发送
-#define BYTE0(dwTemp)       ( *( (char *)(&dwTemp)		) )
+#define BYTE0(dwTemp)       ( *( (char *)(&dwTemp)	  ) )
 #define BYTE1(dwTemp)       ( *( (char *)(&dwTemp) + 1) )
 #define BYTE2(dwTemp)       ( *( (char *)(&dwTemp) + 2) )
 #define BYTE3(dwTemp)       ( *( (char *)(&dwTemp) + 3) )
